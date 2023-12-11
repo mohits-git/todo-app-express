504c62a394b82c32e3faed30c54cd1fd76cea99e
Mohit <152606488+mohits-git@users.noreply.github.com>
2023-12-11 20:54:16 +0530

    Made the index.js look good with the help of chatGPT

--- index.js
+++ index.js
@@ -3,51 +3,74 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const ejs = require('ejs');
-const fs = require('fs');
+const fs = require('fs').promises; 
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/todo', (req, res) => {
-    let todoData = fs.readFileSync('./todoData.json', 'utf-8');
-    todoData = JSON.parse(todoData);
-    res.render('todo', {todoData});
-})
+async function readTodoData() {
+    try {
+        const data = await fs.readFile(path.join(__dirname, 'todoData.json'), 'utf-8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('Error reading todoData.json:', error.message);
+        return {};
+    }
+}
+
+async function writeTodoData(todoData) {
+    try {
+        await fs.writeFile(path.join(__dirname, 'todoData.json'), JSON.stringify(todoData));
+    } catch (error) {
+        console.error('Error writing todoData.json:', error.message);
+    }
+}
+
+app.get('/todo', async (req, res) => {
+    const todoData = await readTodoData();
+    res.render('todo', { todoData });
+});
+
+
+app.post('/todo', async (req, res) => {
+    const { todo } = req.body;
+    let todoData = await readTodoData();
+
+    if (!todoData[todo.username]) {
+        todoData[todo.username] = [];
+    }
+
+    todoData[todo.username].push(todo.task);
+
+    await writeTodoData(todoData);
+
+    res.redirect('/todo');
+});
 
 app.get('/todo/new', (req, res) => {
     res.render('newTodo')
 })
 
-app.post('/todo', async (req, res) => {
-    const { todo } = req.body;
-    let todoData = fs.readFileSync('./todoData.json', 'utf-8')
-    todoData = JSON.parse(todoData);
-    if(!todoData[todo['username']]){
-        todoData[todo['username']] = [];
-    }
-    todoData[todo['username']].push(todo['task']);
-    todoData = JSON.stringify(todoData);
-    fs.writeFileSync('./todoData.json', todoData);
-    res.redirect('/todo');
-})
-
-app.post('/todo/delete', async(req, res) => {
-    const { username, index} = req.body;
-    let todoData = fs.readFileSync('./todoData.json', 'utf-8')
-    todoData = JSON.parse(todoData);
-    todoData[username].splice(index-1, 1);
-    todoData = JSON.stringify(todoData);
-    fs.writeFileSync('./todoData.json', todoData);
-    res.redirect('/todo');
-
-})
 app.get('/todo/delete', (req, res) =>{
     res.render('deleteTodo');
 })
 
-app.listen(port);
+app.post('/todo/delete', async(req, res) => {
+    const { username, index} = req.body;
 
+    let todoData = await readTodoData();
+
+    todoData[username].splice(index-1, 1);
+
+    await writeTodoData(todoData);
+
+    res.redirect('/todo');
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
 //OUR TODO DATA is like this...
     //{
         //    "username" : [
